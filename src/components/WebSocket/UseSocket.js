39ff8c576393bcea6a652f6c1bb4701fdc9843c2
Hakej <michaluszak97@gmail.com>
2023-02-06 22:58:36 +0100

    Removed ignored files from remote

--- src/components/WebSocket/UseSocket.js
+++
@@ -1,8 +0,0 @@
-import { SocketContext } from "./SocketContext";
-import { useContext } from "react";
-
-export const useSocket = () => {
-    const socket = useContext(SocketContext);
-
-    return socket;
-};
